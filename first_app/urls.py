5fdd87bb2ff0bd1b072ef5206647d449de8399df
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-17 22:25:20 +0600

    Added the signup section for a user

---
+++ first_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    # path('user_login/', views.user_login, name='user_login'),
+]
--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from first_app.forms import RegistrationForm
+from django.contrib import messages
 # Create your views here.
+def home(request):
+    return render(request, './home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            print (form.cleaned_data)
+            messages.success(request, 'Signup Successful')
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, './signup.html', {'form': form})
+
+
+            
