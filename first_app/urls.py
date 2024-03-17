e3880af1dd86b180151e837064f3b872d6dc4865
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-17 22:49:51 +0600

    Added the login section for user

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    # path('user_login/', views.user_login, name='user_login'),
+    path('user_login/', views.user_login, name='user_login'),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from first_app.forms import RegistrationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm
 # Create your views here.
 def home(request):
     return render(request, './home.html')
@@ -18,4 +20,20 @@ def signup(request):
     return render(request, './signup.html', {'form': form})
 
 
+def user_login(request):  
+    if request.method == 'POST':
+        form = AuthenticationForm(request.user, data=request.POST)
+        
+        if form.is_valid():
+            name = form.cleaned_data['username']
+            user_pass = form.cleaned_data['password']
             
+            user = authenticate(username = name, password = user_pass)
+            
+            if user is not None:
+                messages.success(request, 'Logged In Successfully')
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, './user_login.html', {'form': form})
